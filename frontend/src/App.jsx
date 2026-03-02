cafb1d3af21dfd70fc832f173ca7672812c43d31
Satyam117-web <sd4053224@gmail.com>
2026-03-02 21:39:33 +0530

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <div className='min-h-screen '>
+     <div className='flex flex-col justify-center items-center '>
+      <img src="/AI.png" className='w-[40%] cursor-pointer ' />
+     </div>
+      <p className='text-6xl font-bold flex justify-center'>Chat AI</p>
+    </div>
+    </>
+  )
+}
+
+export default App
