3f2f4cd99a4ee90836f7fff12c9e3649760ade7a
Nikita-1401 <nikitadtoasd@gmail.com>
2026-03-05 13:59:33 +0530

    msg

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -4,9 +4,9 @@ const Chat = () => {
   return (
     <> 
     <div className='min-h-screen bg-amber-100 flex  '>
-        <div className='w-[80%] h-[100vh] '>left</div>
+        <div className='w-[80%] h-[100vh] '>Left</div>
         <div className='w-0.5 h-[100vh] bg-black m-2'></div>
-        <div className='w-[20%] h-[100vh] bg-amber-100'>right</div>
+        <div className='w-[20%] h-[100vh] bg-amber-100'>Right</div>
     </div>
     </>
   )
