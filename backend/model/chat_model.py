2100d4084b68c9229f1cf04db57f1864fad2cb5b
sk-235648 <skgupta1242005@gmail.com>
2026-03-06 20:42:52 +0530

    chatbot

--- backend/model/chat_model.py
+++ backend/model/chat_model.py
@@ -1,23 +1,36 @@
 import os
 from google import genai
+from openai import OpenAI
+from dotenv import load_dotenv
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-
+# client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+load_dotenv()
 client = OpenAI(
-     api_key=os.getenv"GEMINI_API_KEY",
-     base_url=os.getenv"BASE_URL"
+     api_key=os.getenv("GEMINI_API_KEY"),
+     base_url=os.getenv("BASE_URL")
 )
-
 def chat_model():
     print("ChatAI: Hello!! I''m your virtual Chat Partner, Type to converse")
     print("Type (quit) to quit ")
 
     user_input = input("You: ")
 
-    response = client.models.generate_content(
+    # response = client.models.generate_content(
+    #     model = "gemini-3-flash-preview",
+    #     contents = user_input
+    # )
+
+    response = client.chat.completions.create(
         model = "gemini-3-flash-preview",
-        contents = user_input
+        messages =[
+            {
+                "role" :"user", "content":" Thankyou for your suggestions"
+            }
+        ]
     )
+
+    # print(response.text)
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     chat_model()
