d4f2e2d597fe58523f230b4ce95a3fc05a463f4c
sk-235648 <skgupta1242005@gmail.com>
2026-03-06 12:07:29 +0530

    backend start

---
+++ backend/model/chat_model.py
@@ -0,0 +1,23 @@
+import os
+from google import genai
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+client = OpenAI(
+     api_key=os.getenv"GEMINI_API_KEY",
+     base_url=os.getenv"BASE_URL"
+)
+
+def chat_model():
+    print("ChatAI: Hello!! I''m your virtual Chat Partner, Type to converse")
+    print("Type (quit) to quit ")
+
+    user_input = input("You: ")
+
+    response = client.models.generate_content(
+        model = "gemini-3-flash-preview",
+        contents = user_input
+    )
+
+if __name__ == "__main__":
+    chat_model()
---
+++ backend/readme.md
@@ -0,0 +1,12 @@
+# Create virtual environment
+`python -m venv venv`
+
+# Activate it
+`venv\Scripts\activate`
+
+`python -m pip install --upgrade pip`
+
+`pip install flask`
+
+`pip freeze > requirements.txt`
+
