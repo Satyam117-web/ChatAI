06862bb23e472a4d7651e4b6f75abe6b02654501
sk-235648 <skgupta1242005@gmail.com>
2026-03-06 20:59:20 +0530

    gen model

--- backend/model/chat_model.py
+++ backend/model/chat_model.py
@@ -1,36 +1,42 @@
 import os
 from google import genai
-from openai import OpenAI
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-# client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-load_dotenv()
-client = OpenAI(
-     api_key=os.getenv("GEMINI_API_KEY"),
-     base_url=os.getenv("BASE_URL")
-)
+load_dotenv(find_dotenv())
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+# client = OpenAI(
+#      api_key=os.getenv"GEMINI_API_KEY",
+#      base_url=os.getenv"BASE_URL"
+# )
+
 def chat_model():
     print("ChatAI: Hello!! I''m your virtual Chat Partner, Type to converse")
     print("Type (quit) to quit ")
 
     user_input = input("You: ")
 
-    # response = client.models.generate_content(
-    #     model = "gemini-3-flash-preview",
-    #     contents = user_input
-    # )
-
-    response = client.chat.completions.create(
+    response = client.models.generate_content(
         model = "gemini-3-flash-preview",
-        messages =[
-            {
-                "role" :"user", "content":" Thankyou for your suggestions"
-            }
-        ]
+        contents = user_input
     )
-
-    # print(response.text)
-    print(response.choices[0].message.content)
-
+    print(response.text)
 if __name__ == "__main__":
     chat_model()
+
+# import os
+
+# from dotenv import load_dotenv, find_dotenv
+# from google import genai
+
+# 1. This is the crucial step you are likely missing!
+# It searches for your .env file and loads the keys into memory.
+# load_dotenv(find_dotenv())
+
+# # The client gets the API key from the environment variable `GEMINI_API_KEY`.
+# client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+# response = client.models.generate_content(
+#     model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+# )
+# print(response.text)
